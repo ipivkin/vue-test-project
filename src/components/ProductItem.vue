70f685ccf644750c103ffeee3078a3f6610bdc55
ipivkin <rivezv@gmail.com>
2021-09-04 22:05:07 +0300

    4.4 Changed color prop.

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -17,14 +17,14 @@
     <ul class="colors colors--black">
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-6" value="#222" checked="">
+          <input class="colors__radio sr-only" type="radio" value="#222" v-model="color">
           <span class="colors__value" style="background-color: #222;">
                   </span>
         </label>
       </li>
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-6" value="#73B6EA">
+          <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="color">
           <span class="colors__value" style="background-color: #73B6EA;">
                   </span>
         </label>
@@ -35,6 +35,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      color: '#73B6EA'
+    }
+  },
   props: ['productLabel', 'productPrice', 'productImageLink']
 }
 </script>
